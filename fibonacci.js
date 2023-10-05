1677d2529ee95e8b54a957cd0d6b2a9fb2861f0c
seanmac-dev <smccart3@citadel.edu>
2023-10-04 22:37:57 -0400

    complete assignment with all correct test cases

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-  return 0;
-}
+  let num1 = 0;
+  let num2 = 1;
 
+  for (i = 0; i < num; i++) {
+    let sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num1;
+}
+console.log(fibonacci(3))
 module.exports = fibonacci;
