19ec88434d20e863df1a13049d3ffb098d16b09c
seanmac-dev <smccart3@citadel.edu>
2023-10-05 09:39:52 -0400

    fix for loop

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,7 @@ function fibonacci(num) {
   let num1 = 0;
   let num2 = 1;
 
-  for (i = 0; i < num; i++) {
+  for (let i = 0; i < num; i++) {
     let sum = num1 + num2;
     num1 = num2;
     num2 = sum;
